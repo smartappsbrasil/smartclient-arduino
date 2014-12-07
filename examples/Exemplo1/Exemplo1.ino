503af3fe0fe8da124b2ba38d65259403dbb09fcb
Cleiton Fermino <cfermino@gmail.com>
2014-12-07 20:11:14 -0200

    Update Exemplo1.ino

--- examples/Exemplo1/Exemplo1.ino
+++ examples/Exemplo1/Exemplo1.ino
@@ -1,48 +1,43 @@
 //Smartapps Libs:
 #include <Base642.h>
 #include <SmartClient.h>
-
-
 //Ethernet Libs:
 #include <SPI.h>
 #include <Ethernet.h>
+
 //Definicao endereco MAC do Ethernet Shield:
 //byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xED };
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xED };
 //Definicoes do usuário na Smartapps:
 char server[] = "www.smartapps.com.br";              //Endereco Smartapps.
-char login[] = "Login";   //Login do usuario: Encontra-se na plataforma.
-char password[] = "Password";            //Senha do usuário: Encontra-se na plataforma.
+char login[] = "7a4e894ab12b917aeb0f33b7e61139b8";   //Login do usuario: Encontra-se na plataforma.
+char password[] = "AG8BPFRlUmtfNARmD28=";            //Senha do usuário: Encontra-se na plataforma.
 char app[] = "controls";                             //Nome do aplicativo utilizado.
-char schema[] = "Schema";  //Schema do aplicativo: Normalmente o mesmo do usuário, só muda se for utilizado um aplicativo que foi compartilhado por outro usuário, neste caso é o login do outro usuário.
+char schema[] = "7a4e894ab12b917aeb0f33b7e61139b8";  //Schema do aplicativo: Normalmente o mesmo do usuário, só muda se for utilizado um aplicativo que foi compartilhado por outro usuário, neste caso é o login do outro usuário.
 char caminhoPost[] = "variaveis_valores/insert";
+char* resultado = "";
+
 SmartClient smart; 
 //Variaveis do Exemplo 1;
 String PostData = "";
+String result="";
 //Variaveis usadas pela funcao get_free_memory(). Muito util, principalmente com o Arduino Uno.
 extern int __bss_end;                                
 extern void *__brkval;                               
 void setup() {
+
+  get_free_memory();
   Serial.begin(9600);
-  Serial.print("Free Memory: ");
-  Serial.println(get_free_memory());
-  Serial.println("Starting Ethernet");
   config_rede();
   smart.connect(server, login, password);
+  delay(2000);
+  resultado = smart.getData(server,login,password,app,"variaveis_valores",schema,"_last");
+  Serial.println(resultado);
 }
 void loop() {
-    PostData = "variavel=3&datetime=0&valor="+ String(random(0,100));  //Gera valor randomico a cada loop e adiciona a variavel escolhida.
-    smart.send(true, server, app, schema, caminhoPost, PostData);    //Envia informacoes para plataforma.
-    delay(5000);  //Informacao é enviada a cada 5 segundos.
+    
+}
 
-}
-void config_rede(){
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    for(;;)
-      ;
-  }
-}
 //Funcao get_free_memory(): 
 int get_free_memory()
 {
@@ -53,6 +48,5 @@ int get_free_memory()
     free_memory = ((int)&free_memory) - ((int)__brkval);
   return free_memory;
 }
-
 
 
