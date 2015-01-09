38d1f5d30c8a0ebff562deac9c03625cc74f965e
Cleiton Fermino <cfermino@gmail.com>
2015-01-09 01:13:46 -0200

    Update Exemplo1.ino

--- examples/Exemplo1/Exemplo1.ino
+++ examples/Exemplo1/Exemplo1.ino
@@ -1,43 +1,49 @@
-//Smartapps Libs:
-#include <Base642.h>
 #include <SmartClient.h>
-//Ethernet Libs:
 #include <SPI.h>
 #include <Ethernet.h>
-
-//Definicao endereco MAC do Ethernet Shield:
-//byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xED };
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xED };
 //Definicoes do usuário na Smartapps:
 char server[] = "www.smartapps.com.br";              //Endereco Smartapps.
-char login[] = "7a4e894ab12b917aeb0f33b7e61139b8";   //Login do usuario: Encontra-se na plataforma.
-char password[] = "AG8BPFRlUmtfNARmD28=";            //Senha do usuário: Encontra-se na plataforma.
+char login[] = "";   //Login do usuario: Encontra-se na plataforma.
+char password[] = "=";            //Senha do usuário: Encontra-se na plataforma.
 char app[] = "controls";                             //Nome do aplicativo utilizado.
-char schema[] = "7a4e894ab12b917aeb0f33b7e61139b8";  //Schema do aplicativo: Normalmente o mesmo do usuário, só muda se for utilizado um aplicativo que foi compartilhado por outro usuário, neste caso é o login do outro usuário.
+char schema[] = "";  //Schema do aplicativo: Normalmente o mesmo do usuário, só muda se for utilizado um aplicativo que foi compartilhado por outro usuário, neste caso é o login do outro usuário.
 char caminhoPost[] = "variaveis_valores/insert";
-char* resultado = "";
+char *appSchema;
+char *lastRegister;
+char* sendData;
+int variavel = 11;
+int valor;
+String tecla;
 
 SmartClient smart; 
-//Variaveis do Exemplo 1;
-String PostData = "";
-String result="";
-//Variaveis usadas pela funcao get_free_memory(). Muito util, principalmente com o Arduino Uno.
+String PostData;
 extern int __bss_end;                                
-extern void *__brkval;                               
+extern void *__brkval;   
 void setup() {
-
-  get_free_memory();
   Serial.begin(9600);
   config_rede();
   smart.connect(server, login, password);
-  delay(2000);
-  resultado = smart.getData(server,login,password,app,"variaveis_valores",schema,"_last");
-  Serial.println(resultado);
+    delay(2000);
 }
 void loop() {
-    
+  if(Serial.available() > 0 ){
+    tecla = Serial.readString();
+    Serial.println(Serial.readString());
+    if(tecla == "1"){
+      valor = int(random(0,100));
+      smart.send(server,app, schema, caminhoPost, variavel,valor);
+       delay(2000);
+    }else if(tecla == "2"){
+      appSchema = smart.getSchema(server,login,password);
+      Serial.print("Schema: ");
+      Serial.println(appSchema);    
+    }else{
+      lastRegister = smart.getLastRegister(server,login,password,"controls","",schema,"variaveis_valores","1",2);
+      Serial.println(lastRegister);
+    }
+  } 
 }
-
 //Funcao get_free_memory(): 
 int get_free_memory()
 {
@@ -48,5 +54,7 @@ int get_free_memory()
     free_memory = ((int)&free_memory) - ((int)__brkval);
   return free_memory;
 }
+
+
 
 
